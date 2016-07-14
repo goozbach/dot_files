a36bfeeb18e6643c40a80417737f19c403146a98
Derek Carter <derek@goozbach.com>
2016-07-14 11:50:10 -0600

    Adding acme launcher and updating plan9 stuffs

--- _profile.d/95-plan9.sh
+++ _profile.d/95-plan9.sh
@@ -6,7 +6,8 @@ export PATH
 
 MANPATH=${MANPATH}:${PLAN9}/man
 
-if [[ ${TERM} == 9term ]]
+# inside 9term stuff
+if [[ ${TERM} == 9term || ${TERM} == dumb ]]
 then
   unset PS1
   export PS1='\n[\D{%F:%T} \u@\h \W]\n» '
@@ -14,6 +15,8 @@ then
   alias tree='\tree --charset ascii -n'
   alias grep='grep -Hn'
   alias git='git --no-pager -c color.status=never -c color.branch=never -c color.interactive=never -c color.diff=never'
+  export ANSIBLE_NOCOWS=1
+  export VAGRANT_NO_COLOR=true
   »(){
     eval ${@}
   }
@@ -21,3 +24,29 @@ then
     builtin cd $@ && awd $sysname
   }  
 fi
+
+acme(){
+  export tabstop=2
+  if [[ ! -d /mnt/plumb ]]
+  then
+    mkdir -p /mnt/plumb
+  fi
+  9p ls plumb &>/dev/null || plumber &
+  mount | grep -q '/mnt/plumb' || 9pfuse `namespace`/plumb /mnt/plumb
+  
+  if [[ ! -d /mnt/font ]]
+  then
+    mkdir -p /mnt/font
+  fi
+  9p ls font &>/dev/null || fontsrv &
+  mount | grep -q '/mnt/font' || 9pfuse `namespace`/font /mnt/font
+  
+  if [[ ! -d /mnt/acme ]]
+  then
+    mkdir -p /mnt/acme
+  fi
+  ( $PLAN9/bin/acme -f /mnt/font/LucidaGrande/15a/font -F /mnt/font/Menlo-Bold/15a/font $@; umount /mnt/acme; ) &
+  disown -a
+  sleep 3
+  mount | grep -q '/mnt/acme' || 9pfuse `namespace`/acme /mnt/acme
+}
